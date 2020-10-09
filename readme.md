7e21eb5fac1eab65a1b1f5a340308cf02c456b27
Carsten Jacobsen <car.jacobsen@gmail.com>
2020-10-09 18:06:07 +0200

    feat(initial): initial commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+[![Build Status](https://travis-ci.org/crsten/vue-lazyloader.svg?branch=master&style=flat-square)](https://travis-ci.org/crsten/vue-lazyloader)
+[![npm](https://img.shields.io/npm/dt/vue-lazyloader.svg?style=flat-square)](https://www.npmjs.com/package/vue-lazyloader)
+[![npm](https://img.shields.io/npm/v/vue-lazyloader.svg?style=flat-square)](https://www.npmjs.com/package/vue-lazyloader)
+![npm](https://img.shields.io/npm/l/vue-lazyloader.svg?style=flat-square)
+
+# vue-lazyloader
+
+## Docs
+
+```html
+<LazyLoader>
+  <!-- Your content that will be lazyloaded -->
+</LazyLoader>
+```
+
+```js
+// Import and register
+import LazyLoader from 'vue-lazyloader'
+
+// Vue 2
+Vue.component('LazyLoader', LazyLoader)
+// Vue 3
+app.component('LazyLoader', LazyLoader)
+```
---
+++ src/Component.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="vue-lazyloader">
+    <transition name="fade">
+      <slot v-if="intersected" />
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      intersected: false,
+      observer: null,
+    }
+  },
+  methods: {
+    onIntersect(entries) {
+      const el = entries[0]
+      if (el.isIntersecting) {
+        this.intersected = true
+        this.destroy()
+      }
+    },
+    destroy() {
+      if (!this.observer) return
+      this.observer.disconnect()
+    },
+  },
+  mounted() {
+    this.observer = new IntersectionObserver(this.onIntersect.bind(this), {
+      root: null,
+      threshold: 0,
+    })
+    this.observer.observe(this.$el)
+  },
+  beforeDestroy() {
+    this.destroy()
+  },
+}
+</script>
+
+<style lang="scss">
+.vue-lazyloader {
+  .fade-enter-active,
+  .fade-leave-active {
+    transition-duration: 150ms;
+    transition-timing-function: ease-in;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+}
+</style>
